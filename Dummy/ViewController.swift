02347f338a8c0bd53d1726272f0bc1fd579c34a1
Görkem Önder <gorkem.onder@adesso.com.tr>
2020-05-06 12:58:30 +0300

    Initial Commit

---
+++ Dummy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dummy
+//
+//  Created by Gorkem Onder on 6.05.2020.
+//  Copyright © 2020 Gorkem Onder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
